eac627282623055077c55d6073e7024ea25047a3
Ahmed Sallam <109031729+AhmedSallam100@users.noreply.github.com>
2023-07-09 22:15:52 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -6,53 +6,54 @@ let output = document.querySelector(".img-cont");
 
 downloadVideoBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  if (urlInput.value == "") {
-    alert("Plese Enter Correct URL..!");
+  if (urlInput.value === "") {
+    alert("Please enter a correct URL!");
   } else {
     console.log(`URL: ${urlInput.value}`);
     sendURL(urlInput.value);
   }
 });
 
-// let location2 = window.location.href;
+let sendURL = (URL) => {
+  window.location.href = `https://ahmedsallam100.github.io/Video-Download/index.html?URL=${URL}`;
+};
 
-let sendURL = (URL) =>
-  (location.href = `https://ahmedsallam100.github.io/Video-Download/index.html?URL=${URL}`);
 downloadthumBtn.addEventListener("click", () => {
   var query = urlInput.value;
-  if (urlInput.value == "") {
-    alert("Plese Enter Correct URL..!");
-  }
-  if (query.includes("v=") == true) {
-    var linkRemoved = query.split("v=");
-    var id = linkRemoved[1].split("&")[0];
+  if (query === "") {
+    alert("Please enter a correct URL!");
   } else {
-    var linkRemoved = query.split("/");
-    var id = linkRemoved[3];
-  }
-  if (id != undefined || id != null) {
-    for (var i = 0; i <= 3; i++) {
-      imgs[i].src = `https://img.youtube.com/vi/${id}/mqdefault.jpg`;
+    if (query.includes("v=")) {
+      var linkRemoved = query.split("v=");
+      var id = linkRemoved[1].split("&")[0];
+    } else {
+      var linkRemoved = query.split("/");
+      var id = linkRemoved[3];
     }
-    output.style.display = "flex";
-    output.caption = id;
-  }
-  if (output.style.display == "flex") {
-    document.querySelector(".container").classList.add("active");
+    if (id) {
+      for (var i = 0; i <= 3; i++) {
+        imgs[i].src = `https://img.youtube.com/vi/${id}/mqdefault.jpg`;
+      }
+      output.style.display = "flex";
+      output.caption = id;
+      if (output.style.display === "flex") {
+        document.querySelector(".container").classList.add("active");
+      }
+    }
   }
 });
 
 var preview = (v) => {
-  var id = document.querySelector(".img-cont").caption,
-    a = document.createElement("a");
+  var id = document.querySelector(".img-cont").caption;
+  var a = document.createElement("a");
   a.target = "_blank";
-  if (v == 1) {
+  if (v === 1) {
     a.href = `https://img.youtube.com/vi/${id}/mqdefault.jpg`;
-  } else if (v == 2) {
+  } else if (v === 2) {
     a.href = `https://img.youtube.com/vi/${id}/hqdefault.jpg`;
-  } else if (v == 3) {
+  } else if (v === 3) {
     a.href = `https://img.youtube.com/vi/${id}/sddefault.jpg`;
-  } else if (v == 4) {
+  } else if (v === 4) {
     a.href = `https://img.youtube.com/vi/${id}/maxresdefault.jpg`;
   }
   a.click();
